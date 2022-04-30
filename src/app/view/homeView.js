043284a0880fa914c2a2492408314148eadd1ade
vlcoscar8 <vlcoscar8@gmail.com>
2022-04-30 14:02:41 +0200

    recipes counter included in home tags

--- src/app/view/homeView.js
+++ src/app/view/homeView.js
@@ -8,6 +8,10 @@ const printHome = (listFood) => {
           <img src="${food.img}" alt="${food.name}" class="food__image"/>
           <div class="title__container">
             <h3>${food.name}</h3>
+            <div class="recipes_counter">
+              <i class="fa-solid fa-utensils recipes"></i>
+              <h4>${food.recipes.length}</h4>
+            </div>
           </div>
         </div>
       `;
