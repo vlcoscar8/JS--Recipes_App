63f6db1f67a2bbca931fb86fcb6f791113088b90
vlcoscar8 <vlcoscar8@gmail.com>
2022-05-01 20:46:39 +0200

    style recipe and home updated

--- src/app/view/recipeDetailView.js
+++ src/app/view/recipeDetailView.js
@@ -5,45 +5,43 @@ const printRecipeDetail = (recipe, owner) => {
     <div class="recipe__title">
       <img src="${recipe.img}" alt="${recipe.title}"/>
       <div class="recipe__info">
-        <div class="recipe__title">
           <h2>${recipe.title}</h2>
           <h3>${owner.username}</h3>
-          <div class="recipe__tags">
-            <div class="recipe__tag">
-              <i class="fa-solid fa-person tag"></i>
-              <h4>${recipe.people}</h4>
-            </div>
-            <div class="recipe__tag">
-              <i class="fa-solid fa-fire-flame-simple tag"></i>
-              <h4>${recipe.difficulty}</h4>
-            </div>
-            <div class="recipe__tag">
-              <i class="fa-solid fa-stopwatch tag"></i>
-              <h4>${recipe.time}'</h4>
-          </div>
         </div>
         </div>
       </div>
     </div>
-    <article class="recipe__content">
-      <div class="recipe__category">
-        <h4>${recipe.food}</h4>
-        <i class="fa-solid fa-angles-right"></i>
-        <h4>${recipe.category}</h4>
+    <article class="recipe__options">
+      <div class="recipe__option choosen">
+        <h3>Description</h3>    
       </div>
-      <div class="recipe__description">
-        <h3>Description:</h3>
-        <p>${recipe.description}</p>    
-      </div>
-      <div class="recipe__options">
+      <div class="recipe__option">
         <h3>Ingredients</h3>
-        <i class="fa-solid fa-caret-right arrow"></i>
       </div>
-      <div class="recipe__options">
+      <div class="recipe__option">
         <h3>Steps</h3>
-        <i class="fa-solid fa-caret-right arrow"></i>
       </div>
     </article> 
+    <article class="recipe__tags">
+      <div class="recipe__tag">
+        <i class="fa-solid fa-person tag"></i>
+        <h4>${recipe.people}</h4>
+        <h5>People</h5>
+      </div>
+      <div class="recipe__tag">
+        <i class="fa-solid fa-fire-flame-simple tag"></i>
+        <h4>${recipe.difficulty}</h4>
+        <h5>Difficulty</h5>
+      </div>
+      <div class="recipe__tag">
+        <i class="fa-solid fa-stopwatch tag"></i>
+        <h4>${recipe.time}'</h4>
+        <h5>Time spent</h5>
+      </div>
+    </article>
+    <article class="recipe__content">
+    
+    </article>
     `;
 
     recipeDetail$$.innerHTML = content;
